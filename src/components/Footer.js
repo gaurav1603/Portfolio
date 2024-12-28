c48189eaca5fd1fe73c44f83e05c8a53031246e0
unknown <gsmagar16122003@gmail.com>
2024-12-28 12:30:49 +0530

    First Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Gaurav Magar</h3>
+          <h3>Designed and Developed by Gaurav</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} GM</h3>
